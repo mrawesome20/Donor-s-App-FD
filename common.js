e46c2941229aebbdb30849ce4ebc63b9ec173d97
mrawesome04 <35653738+mrawesome20@users.noreply.github.com>
2021-04-16 21:19:58 +0530

    new updates

--- common.js
+++ common.js
@@ -5,6 +5,15 @@ var registerDonorAPI = API + 'donorSignup';
 var getAllRecieverAPI = API + 'getAllReciever';
 var getLastLoginAPI = API + 'getLastLogin';
 var getDonorDataAPI = API + 'getDonor';
+var updateDonorDataAPI = API + 'updateDonor';
+var  giveDonationAPI = API + 'giveDonation';
+var  newRecieverAPI = API + 'newReciever';
+var  deleteRecieverAPI = API + 'deleteReciever';
+var  getDonationAPI = API + 'getallDonation';
+var  getDataDonationAPI = API + 'getDonationData';
+var  getMonthDonationAPI = API + 'getDonationDataMonth';
+var  getYearDonationAPI = API + 'getDonationDataYear';
+
 
 function SetUserName(username){
     sessionStorage.setItem("username", username)
@@ -22,14 +31,36 @@ function RemoveUserName(){
 //     alert('modalclose');
 // });
 
-function OpenModal(){
-    $('#myModal').removeClass('none');
-    $('#myModal').addClass('block');
+function OpenModal(message = '',title = ''){
+    $('#myModal').removeClass('displaynone');
+    $('#myModal').addClass('displayblock');
+    $('#id-modal-title').empty();
+    $('#id-modal-title').append(title);
+    $('#id-modal-msg').empty();
+    $('#id-modal-msg').append(message);
+    if(!$('#id-modalfooter').hasClass('hidden')){
+        $('#id-modalfooter').addClass('hidden')
+    }
 }
 
 function CloseModal(){
-    $('#myModal').removeClass('block');
-    $('#myModal').addClass('none');
+    $('#myModal').removeClass('displayblock');
+    $('#myModal').addClass('displaynone');
+    if(!$('#id-modalfooter').hasClass('hidden')){
+        $('#id-modalfooter').addClass('hidden')
+    }
+}
+
+function OpenConfirmation(message = '',title = '',Yeslbl = 'Yes',Nolbl = 'No'){
+    $('#myModal').removeClass('displaynone');
+    $('#myModal').addClass('displayblock');
+    $('#id-modal-title').empty();
+    $('#id-modal-title').append(title);
+    $('#id-modal-msg').empty();
+    $('#id-modal-msg').append(message);
+    $('#id-modalfooter').removeClass('hidden');
+    $('#id-modalNo').text(Nolbl);
+    $('#id-modalYes').text(Yeslbl);
 }
 
 window.onbeforeunload = function (e) { RemoveUserName(); };
